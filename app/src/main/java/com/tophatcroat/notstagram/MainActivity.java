03fbdcdb97bc5b4b0386f87eeb28744bbe6b1703
tinoba <tinoba@net.hr>
2015-11-19 22:15:49 +0100

    v

--- app/src/main/java/com/tophatcroat/notstagram/MainActivity.java
+++ app/src/main/java/com/tophatcroat/notstagram/MainActivity.java
@@ -1,13 +1,40 @@
 package com.tophatcroat.notstagram;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+
+import java.util.ArrayList;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    ListView lista;
+    ArrayList<Instagram> listaSlika;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listaSlika = new ArrayList<>();
+        Bitmap icon;
+        Integer[] id = {R.drawable.bakalar, R.drawable.baklava,R.drawable.banana_split,R.drawable.caj};
+        for(Integer broj:id){
+            icon = BitmapFactory.decodeResource(this.getResources(),
+                    broj);
+            listaSlika.add(new Instagram("text broj "+broj,icon));
+        }
+
+
+        lista = (ListView) findViewById(R.id.lista);
+        ListAdapter customAdapter = new CustomAdapterListaSlika(this, listaSlika);
+        lista.setAdapter(customAdapter);
+
     }
 }
